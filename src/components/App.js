4eed97317cdd06da7890f7ec36a39247680e6efd
iddimsangi <adammsangi@gmail.com>
2021-11-30 15:27:36 +0300

    styling form

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,16 @@
-// import logo from './logo.svg';
-import './App.scss';
+import logo from "../images/logo.jpg";
+import Register from "./register/Register";
+import "./App.scss";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <h1>hellow </h1>
+        <img src={logo} className="App-header--logo" alt="logo" />
       </header>
+      <main className="App-main">
+        <Register />
+      </main>
     </div>
   );
 }
---
+++ src/components/register/Register.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "./Register.scss";
+function Register() {
+  return (
+    <div className="register">
+      <h1>register now</h1>
+      <form action="#" className="register-form">
+        <div className="form-group">
+          <input
+            type="text"
+            name="Full name"
+            className="form-input"
+            placeholder="Full Name"
+            id="inputName"
+            value=""
+            required
+          />
+          <label htmlFor="inputName">Full Name</label>
+        </div>
+        <div className="form-group">
+          <input
+            type="email"
+            name="Email Address"
+            className="form-input"
+            placeholder="Email Address"
+            id="inputEmail"
+            value=""
+            required
+          />
+          <label htmlFor="inputEmail">Full Name</label>
+        </div>
+        <a href="#" className="btn">register</a>
+      </form>
+    </div>
+  );
+}
+
+export default Register;
